b91d37a2e926b2ff539cee9fa466bacf4be6dfed
opmile <milenaoli2703@gmail.com>
2026-01-03 12:07:47 -0300

    feat: add EmptyState component for empty tasks case

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,82 @@
-import Container from "./components/Container"
-import Toolbar from "./components/Toolbar"
-import { useState } from "react"
+import Container from "./components/Container";
+import Toolbar from "./components/Toolbar";
+import { useState } from "react";
 import Modal from "./components/ui/Modal";
 import Form from "./components/ui/Form";
 import Section from "./components/Section";
 import Divider from "./components/ui/Divider";
 import SectionColumn from "./components/ui/SectionColumn";
+import EmptyState from "./components/ui/EmptyState";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    { id: 1, title: "Task 1", completed: false },
-    { id: 2, title: "Task 2", completed: true },
-    { id: 3, title: "Task 3", completed: false },
-  ]);
-  
+  const [tasks, setTasks] = useState([]);
+
   const onToggleTask = (taskId) => {
-    setTasks(prevTasks =>
-      prevTasks.map(task =>
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
         task.id === taskId ? { ...task, completed: !task.completed } : task
       )
     );
-  }
+  };
 
-  const completedTasks = tasks.filter(t => t.completed);
-  const pendingTasks = tasks.filter(t => !t.completed);
+  const completedTasks = tasks.filter((t) => t.completed);
+  const pendingTasks = tasks.filter((t) => !t.completed);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
-  }
+  };
 
   const addNewTask = (taskTitle) => {
     const newTask = {
       id: tasks.length + 1,
       title: taskTitle,
-      completed: false
+      completed: false,
     };
     setTasks([...tasks, newTask]);
-  }
+  };
 
   return (
     <>
-    <Container>
-      <Toolbar onClick={toggleModal}/>
+      <Container>
+        <Toolbar onClick={toggleModal} />
 
-      {isModalOpen && (
-        <Modal
-          isOpen={toggleModal}
-          onClose={toggleModal}
-          title="Enter new task description"
-        >
-          <Form 
-            onSubmit={addNewTask}
+        {isModalOpen && (
+          <Modal
+            isOpen={toggleModal}
             onClose={toggleModal}
-          />
-        </Modal>
-      )}
+            title="Enter new task description"
+          >
+            <Form onSubmit={addNewTask} onClose={toggleModal} />
+          </Modal>
+        )}
 
-      <Section>
-        <SectionColumn
-          title="Pending"
-          tasks={pendingTasks}
-          onToggleTask={onToggleTask}
-        />
+        {
+          tasks.length === 0 && (
+            <EmptyState message="No tasks available. Please add a new task." />
+        )}
 
-        <Divider/>
+        { tasks.length > 0 && (
+          <Section>
+            <SectionColumn
+              title="Pending"
+              tasks={pendingTasks}
+              onToggleTask={onToggleTask}
+            />
 
-        <SectionColumn
-          title="Completed"
-          tasks={completedTasks}
-          onToggleTask={onToggleTask}
-        />
-      </Section>
-    </Container>
+            <Divider />
+
+            <SectionColumn
+              title="Completed"
+              tasks={completedTasks}
+              onToggleTask={onToggleTask}
+            />
+          </Section>
+        )}
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 export default function Container({ children }) {
     return (
-        <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md font-primary">
+        <div className="max-w-md mx-auto mt-15 p-6 bg-white rounded-lg shadow-lg font-primary transition-shadow hover:shadow-xl duration-500">
         {children}
         </div>
     )
